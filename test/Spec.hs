ac9a5c901e67b09d84d6368dfc02996c301936bd
Brian Henning <henninb@msn.com>
2020-10-11 10:29:11 -0500

    spec updated

--- test/Spec.hs
+++ test/Spec.hs
@@ -24,29 +24,30 @@ tenClubs = makeCard "Ten Clubs"
 spec :: Spec
 spec =
     describe "Valid Cribbage hand Counts" $ do
-      it "Shows that runs, fifteens, and pairs count is valid" $ do
+      it "Shows that runs, fifteens, and pairs count is valid" $
         scoreTheHand False jackDiamonds [fourClubs, fourDiamonds, fiveClubs, sixHearts] `shouldBe` 14
-      it "Shows that run of four count is valid" $ do
+      it "Shows that run of four count is valid" $
         scoreTheHand False jackDiamonds [fourClubs, sevenHearts, fiveClubs, sixHearts] `shouldBe` 8
-      it "Shows that run of five is valid" $ do
+      it "Shows that run of five is valid" $
         scoreTheHand False eightDiamonds [fourClubs, sevenHearts, fiveClubs, sixHearts] `shouldBe` 9
-      it "Shows a 29 hand count is valid" $ do
+      it "Shows a 29 hand count is valid" $
          scoreTheHand False fiveClubs [fiveSpades, fiveDiamonds, fiveHearts, jackClubs] `shouldBe` 29
-      it "Shows a 28 hand count is valid" $ do
+      it "Shows a 28 hand count is valid" $
          scoreTheHand False jackClubs [fiveSpades, fiveDiamonds, fiveHearts, fiveClubs] `shouldBe` 28
-      it "Shows a 24 hand count is valid" $ do
+      it "Shows a 24 hand count is valid" $
          scoreTheHand False sixHearts [sevenHearts, eightDiamonds, sevenClubs, eightClubs] `shouldBe` 24
-      it "Shows a zero hand count is valid" $ do
+      it "Shows a zero hand count is valid" $
          scoreTheHand False sixHearts [jackDiamonds, eightDiamonds, fourDiamonds, tenClubs] `shouldBe` 0
-      it "Shows a hisNobs hand count is valid" $ do
+      it "Shows a hisNobs hand count is valid" $
          scoreTheHand False fourDiamonds [jackDiamonds, eightDiamonds, tenClubs, sixHearts] `shouldBe` 1
-      it "Shows a four card non crib flush hand count is valid" $ do
+      it "Shows a four card non crib flush hand count is valid" $
          scoreTheHand False tenClubs [jackDiamonds, fourDiamonds, eightDiamonds, sixDiamonds] `shouldBe` 4
-      it "Shows a five card non crib flush hand count is valid" $ do
+      it "Shows a five card non crib flush hand count is valid" $
          scoreTheHand False tenDiamonds [twoDiamonds, fourDiamonds, eightDiamonds, sixDiamonds] `shouldBe` 5
-      it "Shows a four card crib flush hand count is valid" $ do
+      it "Shows a four card crib flush hand count is valid" $
          scoreTheHand True tenClubs [jackDiamonds, fourDiamonds, eightDiamonds, sixDiamonds] `shouldBe` 0
-      it "Shows a five card crib flush hand count is valid" $ do
+      it "Shows a five card crib flush hand count is valid" $
          scoreTheHand True tenDiamonds [twoDiamonds, fourDiamonds, eightDiamonds, sixDiamonds] `shouldBe` 5
+
 main :: IO ()
 main = hspec spec
